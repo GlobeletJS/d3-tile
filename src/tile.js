2f0d6e7f211453e36b13fe64e1e5449554277dcc
Jeshurun Hembd <jjhembd@gmail.com>
2020-09-22 16:44:56 -0500

    Avoid negative zoom

--- src/tile.js
+++ src/tile.js
@@ -13,6 +13,7 @@ function constant(x) {
 }
 
 export default function() {
+  const minZoom = 0;
   let maxZoom = 30;
   let x0 = 0, y0 = 0, x1 = 960, y1 = 500;
   let clampX = true, clampY = true;
@@ -25,7 +26,7 @@ export default function() {
     const scale_ = +scale.apply(this, arguments);
     const translate_ = translate.apply(this, arguments);
     const z = Math.log2(scale_ / tileSize);
-    const z0 = Math.round(Math.min(z + zoomDelta, maxZoom));
+    const z0 = Math.round( Math.min(Math.max(minZoom, z + zoomDelta), maxZoom) );
     const k = Math.pow(2, z - z0) * tileSize;
     const x = +translate_[0] - scale_ / 2;
     const y = +translate_[1] - scale_ / 2;
